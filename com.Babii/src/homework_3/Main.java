fd00bf2332388059f7395f27baaaccbd121fb59f
Amaltheiae <olena.babii.2022@gmail.com>
2022-12-18 00:24:28 +0200

    hi

--- com.Babii/src/homework_3/Main.java
+++ com.Babii/src/homework_3/Main.java
@@ -1,6 +1,7 @@
 package homework_3;
 
 import homework_3.Online_school.FirstYearStudy;
+import homework_3.Online_school.Lecture;
 import homework_3.Online_school.SecondYearStudy;
 import homework_3.Online_school.ThirdYearStudy;
 
@@ -10,16 +11,10 @@ public class Main {
     public static void main(String[] args) {
 
         FirstYearStudy firstYearStudy = new FirstYearStudy();
-
         SecondYearStudy secondYearStudy = new SecondYearStudy();
-
         ThirdYearStudy thirdYearStudy = new ThirdYearStudy();
 
-        System.out.println("Third Year Study:");
-        System.out.println("Name of lecture - " + thirdYearStudy.lecture);
-        System.out.println("Number of Teachers " + thirdYearStudy.teacher);
-        System.out.println("Number of students " + thirdYearStudy.student);
-        System.out.println("Number of homework " + thirdYearStudy.student);
+
 
 
 
