9fa2ba2cd5adbfa2da22d2085e69d8e7b7f7e193
Amaltheiae <olena.babii.2022@gmail.com>
2022-12-17 23:53:49 +0200

    my first commit

--- com.Babii/src/homework_3/Main.java
+++ com.Babii/src/homework_3/Main.java
@@ -11,29 +11,30 @@ public class Main {
 
         FirstYearStudy firstYearStudy = new FirstYearStudy();
 
-        System.out.println("First Year Study:");
+       /* System.out.println("First Year Study:");
         System.out.println("Name of lecture - " + firstYearStudy.lecture);
-//        System.out.println("Number of Teachers " + firstYearStudy.teacher);
-//        System.out.println("Number of students " + firstYearStudy.student);
-//        System.out.println("Number of homework " + firstYearStudy.student);
+        System.out.println("Number of Teachers " + firstYearStudy.teacher);
+       System.out.println("Number of students " + firstYearStudy.student);
+        System.out.println("Number of homework " + firstYearStudy.student);*/
 
 
         SecondYearStudy secondYearStudy = new SecondYearStudy();
 
-        System.out.println("Second Year Study:");
+        /*System.out.println("Second Year Study:");
         System.out.println("Name of lecture - " + secondYearStudy.lecture);
-//        System.out.println("Number of Teachers " + secondYearStudy.teacher);
-//        System.out.println("Number of students " + secondYearStudy.student);
-//        System.out.println("Number of homework " + secondYearStudy.student);
+        System.out.println("Number of Teachers " + secondYearStudy.teacher);
+        System.out.println("Number of students " + secondYearStudy.student);
+        System.out.println("Number of homework " + secondYearStudy.student);*/
 
 
         ThirdYearStudy thirdYearStudy = new ThirdYearStudy();
 
-        System.out.println("Third Year Study:");
+        /*System.out.println("Third Year Study:");
         System.out.println("Name of lecture - " + thirdYearStudy.lecture);
-//        System.out.println("Number of Teachers " + thirdYearStudy.teacher);
-//        System.out.println("Number of students " + thirdYearStudy.student);
-//        System.out.println("Number of homework " + thirdYearStudy.student);
+        System.out.println("Number of Teachers " + thirdYearStudy.teacher);
+        System.out.println("Number of students " + thirdYearStudy.student);
+        System.out.println("Number of homework " + thirdYearStudy.student);
+         */
 
 
     }
