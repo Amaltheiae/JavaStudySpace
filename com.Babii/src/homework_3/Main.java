2719563791a73274ec217048f976ba896b747b11
Amaltheiae <olena.babii.2022@gmail.com>
2022-12-18 00:29:42 +0200

    hi

--- com.Babii/src/homework_3/Main.java
+++ com.Babii/src/homework_3/Main.java
@@ -13,6 +13,7 @@ public class Main {
         FirstYearStudy firstYearStudy = new FirstYearStudy();
         SecondYearStudy secondYearStudy = new SecondYearStudy();
         ThirdYearStudy thirdYearStudy = new ThirdYearStudy();
+        Lecture lectureOne = new Lecture();
 
 
 
