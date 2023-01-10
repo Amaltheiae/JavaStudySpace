809692b18c3af35f4879aa4bf23d0d1c57753882
Amaltheiae <olena.babii.2022@gmail.com>
2023-01-11 01:35:42 +0200

    hw8

---
+++ com.BabiiO/src/hw9/Lecture.java
@@ -0,0 +1,26 @@
+package hw9;
+import java.util.Arrays;
+
+public class Lecture {
+
+
+    private final Marks[] marks = new Marks[]{new Marks(99), new Marks(82), new Marks(91),
+            new Marks(76), new Marks(100), new Marks(95)};
+
+
+    public void showLecture() {
+        System.out.println(Arrays.toString(marks));
+        System.out.println(marks[2]);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ com.BabiiO/src/hw9/Marks.java
@@ -0,0 +1,32 @@
+package hw9;
+
+public class Marks {
+
+    int marks;
+
+    public Marks(final int marks) {
+        this.marks = marks;
+    }
+
+    public Marks() {
+    }
+
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+                "Students marks = " + marks +
+                '}';
+    }
+
+}
+
