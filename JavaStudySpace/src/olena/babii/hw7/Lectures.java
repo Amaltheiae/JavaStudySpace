8fd14fdf8e94de6d950d028f4e9f49857891bb02
Amaltheiae <olena.babii.2022@gmail.com>
2023-02-06 22:52:28 +0200

    from hw7

---
+++ JavaStudySpace/src/olena/babii/hw7/Lectures.java
@@ -0,0 +1,20 @@
+package olena.babii.hw7;
+import java.util.Scanner;
+
+public class Lectures {
+
+    public int id = Course.countCourses;
+
+    public Lectures(int id) {
+        this.id = id;
+    }
+
+
+    /*public int id = Course.id;
+
+    public Lectures(int id) {
+        this.id = id;
+    }*/
+
+
+}
