2fa0f17d01364bdbd0d11143ef840c29b866972d
Amaltheiae <olena.babii.2022@gmail.com>
2023-02-06 22:46:09 +0200

    from hw6

---
+++ JavaStudySpace/src/olena/babii/hw6/Lecture.java
@@ -0,0 +1,15 @@
+package olena.babii.hw6;
+
+public class Lecture {
+
+    public static int count;
+    public String lecture;
+    public int id;
+
+
+    public Lecture(String lecture) {
+        this.lecture = lecture;
+        count++;
+        this.id = count;
+    }
+}
